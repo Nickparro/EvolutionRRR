a7bf8e65d640e3d0c29d3a9e7d65955c189746f2
Nicolas Parroquiano <nicolasparroquiano@gmail.com>
2024-04-04 15:36:46 -0500

    Planet Weather States Animations
    Planet Weather Animations
    Planet Water Animation
    FIsh and Lizard Animations

---
+++ Assets/Scripts/PlanetStates.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetStates : MonoBehaviour
+{
+    private Renderer rendererObject;
+    private Color targetColor;
+    public float transitionSpeed;
+    public Color dryColor;
+    public Color fireColor;
+    public Color iceColor;
+    public bool isHot;
+    public bool isCold;
+    public bool isPerfect;
+    void Start()
+    {
+        rendererObject = GetComponent<Renderer>();
+        isPerfect = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeWeatherState(targetColor);
+    }
+
+    public void ChangeWeatherState(Color colorToChange)
+    {
+        if(isHot)
+        {
+            isCold = false;
+            isPerfect = false;
+            targetColor = fireColor;
+        } else if (isCold)
+        {
+            isHot = false;
+            isPerfect = false;
+            targetColor = iceColor;
+        } else 
+        {
+            isPerfect = true;
+            isHot = false;
+            isCold = false;
+            targetColor = dryColor;
+        }
+        
+        // Asegúrate de que rendererObject no sea null
+        if (rendererObject != null)
+    {
+        // Itera sobre todos los materiales y cambia su color.
+        for (int i = 0; i < rendererObject.materials.Length; i++)
+        {
+            rendererObject.materials[i].color = Color.Lerp(rendererObject.materials[i].color, targetColor, transitionSpeed * Time.deltaTime);
+        }
+    }
+    }
+
+}
